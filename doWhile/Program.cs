9a29fbda0b8cbfb3fffc90df410668d96c03ba12
BURAK DÜLGEROĞLU <dulgerogluburak04@gmail.com>
2025-05-12 00:29:29 +0700

    doWhilePratik

---
+++ doWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿int sayac = 0;
+Console.Write("Lütfen bir limit değeri giriniz.");
+int limit = int.Parse(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım.");
+    sayac++;
+} while (sayac <= limit);
+
+//doWhile döngüsünde "do" içindeki komut "while" içindeki koşul geçersiz olsa bile en az 1 kez çalışarak ekrana Ben bir Patika'lıyım yazdıracaktır
+//bu nedenle örnek input -5 girildiğinde geçersiz değer girilmesine rağmen do komutu 1 kez çalışır.
+
+
+Console.Write("Lütfen bir limit değeri giriniz.");
+int sayac = 0;
+int limit = int.Parse(Console.ReadLine());
+
+while (sayac<=limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım.");
+    sayac++;
+}
+
+//while döngüsünde koşul başlangıçta kontrol edilir ve sağlanmadıysa döngü hiç çalışmaz.
+//bu nedenle geçersiz input olan -5 girildiğinde döngü hiç çalışmaz.
